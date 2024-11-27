38279e4f3cc10bb9062e79dd7fdb3bd8dac0a219
IgorLevch <igorlevvv@gmail.com>
2024-11-27 19:02:48 +0300

    The first execution of the project.

---
+++ src/main/java/com/example/spBDCN2/SpBdcn2Application.java
@@ -0,0 +1,13 @@
+package com.example.spBDCN2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBdcn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBdcn2Application.class, args);
+	}
+
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+  create table t_user
+  (
+
+        id serial primary key,
+        c_username varchar unique not null
+        );
